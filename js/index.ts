84eaccac2e188033b9a84e752999441f46f5a553
Ben Foxall <benfoxall@gmail.com>
2021-01-30 15:31:53 +0000

    Add landing page

--- js/index.ts
+++ js/index.ts
@@ -1,11 +1,41 @@
 
-// A web server that listens to 8080
 import express from 'express';
+import bodyParser from 'body-parser';
+import {join} from 'path'
 
 const app = express();
 
-app.get('/', (req, res) => {res.send("hello from node!")})
+const ROOT = 'js.bottle.remotehack.space';
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.all('/', urlencodedParser, (req, res, next) => {
+
+    const {host} = req.headers;
+
+    if(host === ROOT) {
+        // 
+        if(req.method === 'GET') {
+            res.sendFile(join(__dirname, '/../www/landing.html'))
+            return;
+        }
+
+        if(req.method === 'POST') {
+            res.send(`TODO: CREATE SUBDOMAIN: ${req.body.subdomain}`)
+        
+            return;
+        }
+
+    }
+
+    next();
+
+})
+
+app.all('*', (req, res) => {
+    const {host} = req.headers;
+    res.send(`(NODE) FALLBACK2 - ${host}`)
+})
 
 app.listen(8000, () => {
     console.log("started")
