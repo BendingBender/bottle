712c4aeb0bd4d19fffda1da3f60064a22a6df84a
Ben Foxall <benfoxall@gmail.com>
2021-01-30 12:44:00 +0000

    Typescriptify

--- js/index.ts
+++ js/index.ts
@@ -1,9 +1,10 @@
 
 // A web server that listens to 8080
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {res.send("hello")})
+
 
 app.listen(8000);
