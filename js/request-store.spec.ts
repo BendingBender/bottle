d29300683a6a9fcbb386559b65f8239eea8f0239
Ben Foxall <benfoxall@gmail.com>
2021-01-30 15:25:33 +0000

    Add request store

--- js/request-store.spec.ts
+++ js/request-store.spec.ts
@@ -1,37 +1,102 @@
-import { RequestsStore, NoSuchSubdomain } from './request-store';
+import { RequestsStore, NoSuchSubdomainError } from './request-store';
 import { promises as fs } from 'fs';
+import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import os from 'os';
 import path from 'path';
 
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
 describe('RequestsStore', function() {
-  let dir: string | undefined = undefined;
+
+  const subdomain = 'my-subdomain';
+
+  let dir: string | undefined;
+  let store: RequestsStore | undefined;
   beforeEach(async function() {
     dir = await fs.mkdtemp(path.join(os.tmpdir(), 'foo-'));
 
-    console.log(dir);
-  });
-  afterEach(async function() {
+    store = new RequestsStore({
+      storeDir: dir
+    });
+
     if (dir === undefined) {
-      throw "Error setting up tests - can't tear down";
+      throw "Error setting up tests - couldn't create temp directory";
     }
-    await fs.rmdir(dir);
+    if (store === undefined) {
+      throw "Error setting up tests - couldn't create store";
+    }
+  });
+
+  afterEach(async function() {
+    await fs.rmdir(dir!, { recursive: true });
   });
 
   describe('#write()', function() {
     it("write doesn't work if subdomain not yet created", async function() {
-      if (dir === undefined) {
-        throw "Error setting up tests";
-      }
-      const store = new RequestsStore({
-        storeDir: dir
-      });
 
-      expect(store.write({
-          subdomain: 'my-subdomain',
+      await expect(store!.write({
+          subdomain: subdomain,
           timestamp: '2020-01-01',
           content: 'my content'
-      })).to.be.rejectedWith(NoSuchSubdomain);
+      })).to.be.rejectedWith(NoSuchSubdomainError);
+
+    });
+    it("write works if subdomain not yet created", async function() {
+
+      await store!.createStore({ subdomain });
+
+      await expect(store!.write({
+          subdomain,
+          timestamp: '2020-01-01',
+          content: 'my content'
+      })).to.not.be.rejected;
+
+    });
+    it("write puts out the correct content", async function() {
+
+      await store!.createStore({ subdomain });
+
+      await store!.write({
+          subdomain,
+          timestamp: '2020-01-01',
+          content: 'my content1'
+      });
+      await store!.write({
+          subdomain,
+          timestamp: '2020-01-02',
+          content: 'my content2'
+      });
+      await store!.write({
+          subdomain,
+          timestamp: '2020-01-03',
+          content: 'my content3'
+      });
+      await store!.write({
+          subdomain,
+          timestamp: '2020-01-04',
+          content: 'my content4'
+      });
+      await store!.write({
+          subdomain,
+          timestamp: '2020-01-05',
+          content: 'my content5'
+      });
+
+
+      expect(await store!.read({subdomain}))
+        .to.equal(`-----2020-01-01
+bXkgY29udGVudDE=
+-----2020-01-02
+bXkgY29udGVudDI=
+-----2020-01-03
+bXkgY29udGVudDM=
+-----2020-01-04
+bXkgY29udGVudDQ=
+-----2020-01-05
+bXkgY29udGVudDU=
+`);
 
     });
   });
--- js/request-store.ts
+++ js/request-store.ts
@@ -1,21 +1,21 @@
 import { promises as fs, constants } from 'fs';
 import { EOL } from 'os';
 
-class NoSuchSubdomain extends Error {
+export class NoSuchSubdomainError extends Error {
 
   public subdomain: string;
 
   constructor ({ subdomain }: { subdomain : string }) {
     super(`No such domain ${subdomain}`);
     this.subdomain = subdomain;
-    Error.captureStackTrace(this, NoSuchSubdomain);
+    //Error.captureStackTrace(this, NoSuchSubdomainError);
   }
 }
 
 /**
  * @class RequestsStore Stores and reads requests for a domain.
  */
-class RequestsStore {
+export class RequestsStore {
   private storeDir: string;
 
   constructor({ storeDir = "data" } : { storeDir?: string } = {}) {
@@ -23,7 +23,11 @@ class RequestsStore {
   }
 
   async createStore({ subdomain } : { subdomain : string }) {
+
+    await fs.mkdir(this.storeDir, { recursive: true });
+
     const fileName = this.getFileNameFromSubdomain(subdomain);
+    await fs.writeFile(fileName, '', 'utf-8');
   }
 
   async write({ subdomain, timestamp, content } : { subdomain : string, timestamp : string, content : string }): Promise<void> {
@@ -34,16 +38,16 @@ class RequestsStore {
     let file = undefined;
 
     try {
-      file = await fs.open(fileName, constants.O_APPEND);
+      file = await fs.open(fileName, constants.O_WRONLY | constants.O_APPEND);
       await file.write(toAppend, 0, 'utf-8');
     } catch (e) {
       if (e.code === 'ENOENT') {
-        throw new NoSuchSubdomain({ subdomain });
+        throw new NoSuchSubdomainError({ subdomain });
       } else {
         throw e;
       }
     } finally {
-      file?.close();
+      await file?.close();
     }
 
   }
@@ -58,8 +62,3 @@ class RequestsStore {
     return `${this.storeDir}/${subdomain}`;
   }
 }
-
-export {
-  RequestsStore,
-  NoSuchSubdomain
-};
